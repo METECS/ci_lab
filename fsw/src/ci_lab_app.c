38e7c7f9701abdad60da255f71cc2712c36e01e3
Jonathan Brandenburg <jonathan.brandenburg@metecs.com>
2020-02-20 17:05:24 +0000

    Address structure alignment errors

--- fsw/src/ci_lab_app.c
+++ fsw/src/ci_lab_app.c
@@ -49,7 +49,12 @@ CFE_SB_PipeId_t    CI_CommandPipe;
 CFE_SB_MsgPtr_t    CIMsgPtr;
 int                CI_SocketID;
 struct sockaddr_in CI_SocketAddress;
-uint8              CI_IngestBuffer[CI_MAX_INGEST];
+/*
+ * CFE_SB_Msg_t is 32-bit aligned because of its data types.
+ * Ensure CI_IngestBuffer is also 32-bit aligned to avoid
+ * alignment issues
+ */
+uint32             CI_IngestBuffer[CI_MAX_INGEST/4];
 CFE_SB_Msg_t *     CI_IngestPointer = (CFE_SB_Msg_t *)&CI_IngestBuffer[0];
 CFE_SB_MsgId_t     PDUMessageID     = 0;
 bool               adjustFileSize   = false;
@@ -294,8 +299,22 @@ void CI_ProcessGroundCommand(void)
 void CI_ReportHousekeeping(void)
 {
     CI_HkTelemetryPkt.SocketConnected = CI_SocketConnected;
-    CFE_SB_TimeStampMsg((CFE_SB_Msg_t *)&CI_HkTelemetryPkt);
-    CFE_SB_SendMsg((CFE_SB_Msg_t *)&CI_HkTelemetryPkt);
+    {
+        /*
+         * Create and use a temporary structure to ensure type alignment
+         */
+        CFE_SB_Msg_t tempMessage;
+        memcpy(&tempMessage, &CI_HkTelemetryPkt, sizeof(tempMessage));
+
+        CFE_SB_TimeStampMsg((CFE_SB_Msg_t *)&tempMessage);
+        CFE_SB_SendMsg((CFE_SB_Msg_t *)&tempMessage);
+
+        /*
+         * Copy the temporary message back to the original source as a good practice
+         * even if not used later
+         */
+        memcpy(&CI_HkTelemetryPkt, &tempMessage, sizeof(tempMessage));
+    }
     return;
 
 } /* End of CI_ReportHousekeeping() */
@@ -549,8 +568,8 @@ void CI_ProcessPDU(void)
     uint8          EntityIdBytes, TransSeqBytes, PduHdrBytes;
     CFE_SB_MsgId_t MessageID = CFE_SB_GetMsgId(CI_IngestPointer);
     bool           sendToSB  = false;
-    uint32 *       checkSumPtr;
-    uint32 *       fileSizePtr;
+    uint8 *        checkSumPtr;
+    uint8 *        fileSizePtr;
 
     if (MessageID == PDUMessageID)
     {
@@ -614,15 +633,21 @@ void CI_ProcessPDU(void)
                         dropEOF  = false;
                     }
 
+		    /*
+		     * checkSumPtr is a uint8_t * to avoid alignment issues
+		     */
                     PduDataPtr += 2;
-                    checkSumPtr = (uint32 *)PduDataPtr;
-                    fileSizePtr = checkSumPtr + 1;
+                    checkSumPtr = (uint8 *)PduDataPtr;
+                    fileSizePtr = checkSumPtr + 4;
 
                     if (corruptChecksum == true)
                     {
                         OS_printf("CI: good checksum = %x\n", (unsigned int)*checkSumPtr);
                         /* Corrupt the checksum */
-                        *checkSumPtr = 0x12345678;
+                        *(checkSumPtr + 0) = 0x12;
+                        *(checkSumPtr + 1) = 0x34;
+                        *(checkSumPtr + 2) = 0x56;
+                        *(checkSumPtr + 3) = 0x78;
                         OS_printf("CI: corrupted checksum = %x\n", (unsigned int)*checkSumPtr);
                         corruptChecksum = false;
                     }
@@ -631,7 +656,7 @@ void CI_ProcessPDU(void)
                     {
                         OS_printf("CI: good file size = %d\n", (int)*fileSizePtr);
                         /* Adjust the file size */
-                        *fileSizePtr += PDUFileSizeAdjustment;
+                        *fileSizePtr += PDUFileSizeAdjustment * 4;
                         OS_printf("CI: adjusted file size = %d\n", (int)*fileSizePtr);
                         adjustFileSize = false;
                     }
